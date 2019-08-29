80afba31cccbb5cbd0c1819ad016bbac74c935b5
Fabio Callegari <fabiocallegari.it@gmail.com>
2019-08-28 21:50:47 -0700

    add webpack config

--- wp-content/plugins/mytheme-blocks/plugin.php
+++ wp-content/plugins/mytheme-blocks/plugin.php
@@ -11,22 +11,23 @@ if( !defined('ABSPATH') ) {
     exit;
 }
 
+function my_theme_blocks_register_block_type($block, $options = array()) {
+    register_block_type('mytheme-blocks/' . $block,
+        array_merge( array(
+            'editor_script' => 'mytheme-blocks-editor-script'
+        ), $options)
+    );
+}
+
 function mytheme_blocks_register() {
 
     wp_register_script(
-        'mytheme-blocks-firstblock-editor-script',
-        plugins_url('blocks/firstblock/index.js', __FILE__),
+        'mytheme-blocks-editor-script',
+        plugins_url('dist/editor.js', __FILE__),
         array('wp-blocks', 'wp-i18n', 'wp-element')
     );
 
-    register_block_type('mytheme-blocks/firstblock',
-    array(
-        'editor_script' => 'mytheme-blocks-firstblock-editor-script',
-        'script',
-        'style',
-        'editor_style'
-    )
-    );
+    my_theme_blocks_register_block_type('firstblock');
 }
 
 add_action('init', 'mytheme_blocks_register');
