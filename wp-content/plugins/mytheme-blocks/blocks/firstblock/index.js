ead7f21c18ab4fdb2ae096fac1bbcf5f05f6b443
Fabio Callegari <fabiocallegari.it@gmail.com>
2019-08-28 18:56:55 -0700

    create first block

---
+++ wp-content/plugins/mytheme-blocks/blocks/firstblock/index.js
@@ -0,0 +1,14 @@
+var registerBlockType = wp.blocks.registerBlockType;
+var __ = wp.i18n.__;
+
+registerBlockType('mytheme-blocks/firstblock', {
+    title: __('First Block', 'mytheme-blocks'),
+    description: __('Our first block', 'mytheme-blocks'),
+    category: 'layout',
+    edit: function() {
+        return 'Editor';
+    },
+    save: function() {
+        return 'Saved content';
+    }
+});
