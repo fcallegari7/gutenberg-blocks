fbc00bbac659ac4fb66a5e1c7ae3c67cda3d9fd5
Fabio Callegari <fabiocallegari.it@gmail.com>
2019-09-25 18:26:43 -0700

    delete and edit img functions

--- wp-content/plugins/mytheme-blocks/src/blocks/teammember/edit.js
+++ wp-content/plugins/mytheme-blocks/src/blocks/teammember/edit.js
@@ -1,8 +1,8 @@
 import { Component } from "@wordpress/element";
-import { RichText, MediaPlaceholder } from "@wordpress/editor";
+import { RichText, MediaPlaceholder, BlockControls, MediaUpload, MediaUploadCheck } from "@wordpress/editor";
 import { __ } from "@wordpress/i18n";
 import { isBlobURL } from "@wordpress/blob";
-import { Spinner, withNotices } from "@wordpress/components";
+import { Spinner, withNotices, Toolbar, IconButton } from "@wordpress/components";
 
 class TeamMemberEdit extends Component {
     componentDidMount() {
@@ -37,44 +37,84 @@ class TeamMemberEdit extends Component {
         const { noticeOperations } = this.props;
         noticeOperations.createErrorNotice(message);
     }
+    removeImage = () => {
+        this.props.setAttributes({
+            id: null,
+            url: '',
+            alt: ''
+        })
+    }
     render() {
         const { className, attributes, noticeUI } = this.props;
-        const { title, info, url, alt } = attributes;
+        const { title, info, url, alt, id } = attributes;
         return (
-            <div className={ className }>
-                {url ? 
-                    <>
-                        <img src={src} alt={alt} />
-                        {isBlobURL(url) && <Spinner />}
-                    </>
-                    : <MediaPlaceholder 
-                        icon="format-image"
-                        onSelect={this.onSelectImage}
-                        onSelectURL={this.onSelectURL}
-                        onError={this.onUploadError}
-                        accept="image/*"
-                        allowedTypes={['image']}
-                        notices={noticeUI}
+            <>
+                <BlockControls>
+                    {url &&
+                        <Toolbar>
+                            {id && 
+                                <MediaUploadCheck>
+                                    <MediaUpload 
+                                        onSelect={ this.onSelectImage }
+                                        allowedTypes={['image']}
+                                        value={ id }
+                                        render={({open}) => {
+                                            return (
+                                                <IconButton 
+                                                    className="components-icon-button components-toolbar__control"
+                                                    label={ __('Remove Image', 'mytheme-blocks') }
+                                                    onClick={ open }
+                                                    icon="edit"
+                                                />
+                                            )
+                                        }}
+                                    />
+                                </MediaUploadCheck>
+                            }
+                            <IconButton 
+                                className="components-icon-button components-toolbar__control"
+                                label={ __('Remove Image', 'mytheme-blocks') }
+                                onClick={ this.removeImage }
+                                icon="trash"
+                            />
+                        </Toolbar>
+                    }
+                </BlockControls>
+                <div className={ className }>
+                    {url ? 
+                        <>
+                            <img src={src} alt={alt} />
+                            {isBlobURL(url) && <Spinner />}
+                        </>
+                        : <MediaPlaceholder 
+                            icon="format-image"
+                            onSelect={this.onSelectImage}
+                            onSelectURL={this.onSelectURL}
+                            onError={this.onUploadError}
+                            accept="image/*"
+                            allowedTypes={['image']}
+                            notices={noticeUI}
+                        />
+                    }
+                    <RichText 
+                        className={'wp-block-mytheme-blocks-teammember__title'}
+                        tagName="h4"
+                        onChange={this.onChangeTitle}
+                        value={title}
+                        placeholder={__('Member Name', 'mytheme-blocks')}
+                        formattingControls={[]}
                     />
-                }
-                <RichText 
-                    className={'wp-block-mytheme-blocks-teammember__title'}
-                    tagName="h4"
-                    onChange={this.onChangeTitle}
-                    value={title}
-                    placeholder={__('Member Name', 'mytheme-blocks')}
-                    formattingControls={[]}
-                />
-                <RichText 
-                    className={'wp-block-mytheme-blocks-teammember__info'}
-                    tagName="p"
-                    onChange={this.onChangeInfo}
-                    value={info}
-                    placeholder={__('Member Info', 'mytheme-blocks')}
-                    formattingControls={[]}
-                />
+                    <RichText 
+                        className={'wp-block-mytheme-blocks-teammember__info'}
+                        tagName="p"
+                        onChange={this.onChangeInfo}
+                        value={info}
+                        placeholder={__('Member Info', 'mytheme-blocks')}
+                        formattingControls={[]}
+                    />
 
-            </div>
+                </div>
+            </>
         )
 
     }
